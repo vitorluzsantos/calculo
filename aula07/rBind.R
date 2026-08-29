c46010301b8fe8d4e513d265a285146e17f993d1
vitorluzsantos <vitor.lsantos@sptech.school>
2026-08-29 16:25:56 -0300

    feat: atividade R de captura

---
+++ aula07/rBind.R
@@ -0,0 +1,37 @@
+# rbind junta dois dataframes
+# read.csv() <- le um arquivo csv 
+
+dfVitor <- read.csv("arquivosCaptura/captura_vitor.csv",
+	header = FALSE,
+	sep = ";",
+	col.names = c("timestamp", "user", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
+)
+
+dfLivia <- read.csv("arquivosCaptura/captura_livia.csv",
+	header = FALSE,
+	sep = ";",
+	col.names = c("timestamp", "user", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
+)
+
+dfRaphael <- read.csv("arquivosCaptura/captura_raphael.csv",
+	header = FALSE,
+	sep = ";",
+	col.names = c("timestamp", "user", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
+)
+
+dfCarlos <- read.csv("arquivosCaptura/captura_carlos.csv",
+	header = FALSE,
+	sep = ";",
+	col.names = c("timestamp", "user", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
+)
+
+dfMatheus <- read.csv("arquivosCaptura/captura_matheus.csv",
+	header = FALSE,
+	sep = ";",
+	col.names = c("timestamp", "user", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
+)
+
+dfGeral <- rbind(dfVitor, dfMatheus, dfCarlos, dfRaphael, dfLivia)
+
+head(dfGeral)
+rm(df)
